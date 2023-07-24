610916b0d543da0a7140e430a8b97ab1d8dec7ee
Гаврилов Димитрий <gavrilov.master@gmail.com>
2023-07-24 18:43:52 +0300

    добавление поиска(не рабочий вариант)

--- app/src/main/java/com/example/channels/MyPagerAdapter.kt
+++ app/src/main/java/com/example/channels/MyPagerAdapter.kt
@@ -1,11 +1,12 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.channels
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 
-
-class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+class MyPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) { // Замените FragmentPagerAdapter на FragmentStatePagerAdapter
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
