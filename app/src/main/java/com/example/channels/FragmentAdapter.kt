3f031ce859c1e671ecc153a1bb5936e1332f225d
Гаврилов Димитрий <gavrilov.master@gmail.com>
2023-07-27 13:09:13 +0300

    Первоначальная верстка MainActivity

---
+++ app/src/main/java/com/example/channels/FragmentAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.channels
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+
+class FragmentAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                AllFragment()
+            }
+            else -> {
+                return FavoritesFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "Все"
+            else -> {
+                return "Избранные"
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/channels/MainActivity.kt
+++ app/src/main/java/com/example/channels/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.channels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Вкладки
+        val viewpagerForTabs = findViewById<ViewPager>(R.id.viewpagerForTabs)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+        val fragmentAdapter = FragmentAdapter(supportFragmentManager)
+
+        viewpagerForTabs.adapter = fragmentAdapter
+        tabs.setupWithViewPager(viewpagerForTabs)
+
+        //
     }
 }
