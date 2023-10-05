eafe32be0b487cb09ed5f4fecdde28b056f5427c
Гаврилов Димитрий <gavrilov.master@gmail.com>
2023-10-05 16:53:51 +0300

    крашлитикс пофикшено аппметрика(пуши не идут)

--- app/src/main/java/com/example/channels/App.kt
+++ app/src/main/java/com/example/channels/App.kt
@@ -1,6 +1,9 @@
 package com.example.channels
 
 import android.app.Application
+import com.yandex.appmetrica.push.firebase.FirebasePushServiceControllerProvider
+import com.yandex.appmetrica.push.hms.HmsPushServiceControllerProvider
+import com.yandex.metrica.push.YandexMetricaPush
 import dagger.hilt.android.HiltAndroidApp
 import io.appmetrica.analytics.AppMetrica
 import io.appmetrica.analytics.AppMetricaConfig
@@ -13,5 +16,10 @@ class App : Application() {
         val config = AppMetricaConfig.newConfigBuilder("005f0b9e-8188-47bc-a5ea-db3c6c66e254").build()
         // Initializing the AppMetrica SDK.
         AppMetrica.activate(this, config)
+        YandexMetricaPush.init(
+            applicationContext,
+            FirebasePushServiceControllerProvider(this),
+            HmsPushServiceControllerProvider(this)
+        )
     }
 }
