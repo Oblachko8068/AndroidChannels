fac72542f0dd58d0b20be8dcf30aa39d34ac73ff
Гаврилов Димитрий <gavrilov.master@gmail.com>
2023-10-12 13:42:38 +0300

    пуши и краши пофикшены

--- app/src/main/java/com/example/channels/App.kt
+++ app/src/main/java/com/example/channels/App.kt
@@ -1,25 +1,23 @@
 package com.example.channels
 
 import android.app.Application
-import com.yandex.appmetrica.push.firebase.FirebasePushServiceControllerProvider
-import com.yandex.appmetrica.push.hms.HmsPushServiceControllerProvider
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import com.yandex.metrica.push.YandexMetricaPush
 import dagger.hilt.android.HiltAndroidApp
-import io.appmetrica.analytics.AppMetrica
-import io.appmetrica.analytics.AppMetricaConfig
+
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        // Creating an extended library configuration.
-        val config = AppMetricaConfig.newConfigBuilder("005f0b9e-8188-47bc-a5ea-db3c6c66e254").build()
+
+        val config: YandexMetricaConfig = YandexMetricaConfig.newConfigBuilder("005f0b9e-8188-47bc-a5ea-db3c6c66e254").build()
         // Initializing the AppMetrica SDK.
-        AppMetrica.activate(this, config)
-        YandexMetricaPush.init(
-            applicationContext,
-            FirebasePushServiceControllerProvider(this),
-            HmsPushServiceControllerProvider(this)
-        )
+        YandexMetrica.activate(applicationContext, config)
+        // Automatic tracking of user activity.
+        YandexMetrica.enableActivityAutoTracking(this)
+
+        YandexMetricaPush.init(this)
     }
 }
