8ffcd49a6e7a0cdd49db66a5fb5bb833791d55a7
Oblachko8068 <nikita00784200@gmail.com>
2023-10-18 22:36:47 +0300

    Добавление межстраничной рекламы

--- app/src/main/java/com/example/channels/MainActivity.kt
+++ app/src/main/java/com/example/channels/MainActivity.kt
@@ -11,11 +11,21 @@ import com.example.channels.fragments.Navigator
 import com.example.di.di.Di
 import com.example.domain.model.Channel
 import com.example.domain.model.Epg
+import com.yandex.mobile.ads.common.AdError
+import com.yandex.mobile.ads.common.AdRequest
+import com.yandex.mobile.ads.common.AdRequestConfiguration
+import com.yandex.mobile.ads.common.AdRequestError
+import com.yandex.mobile.ads.common.ImpressionData
+import com.yandex.mobile.ads.interstitial.InterstitialAd
+import com.yandex.mobile.ads.interstitial.InterstitialAdEventListener
+import com.yandex.mobile.ads.interstitial.InterstitialAdLoadListener
+import com.yandex.mobile.ads.interstitial.InterstitialAdLoader
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), Navigator {
 
+    private var interstitialAd : InterstitialAd? = null
     @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +41,54 @@ class MainActivity : AppCompatActivity(), Navigator {
 
     }
 
+    private fun loadInterAd(){
+        val adRequest = AdRequestConfiguration.Builder("demo-banner-yandex").build()
+        InterstitialAdLoader(this).apply {
+            setAdLoadListener(object : InterstitialAdLoadListener {
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    interstitialAd = ad
+                }
+
+                override fun onAdFailedToLoad(adRequestError: AdRequestError) {
+                    interstitialAd = null
+                }
+            })
+        }.loadAd(adRequest)
+    }
+    private fun showInterAd(){
+        if (interstitialAd != null){
+            interstitialAd?.apply {
+                setAdEventListener(object : InterstitialAdEventListener {
+                    override fun onAdShown() {
+                        interstitialAd = null
+                        loadInterAd()
+                        return
+                    }
+                    override fun onAdFailedToShow(adError: AdError) {
+                        interstitialAd = null
+                        loadInterAd()
+                        return
+                    }
+                    override fun onAdDismissed() {
+                        interstitialAd = null
+                        loadInterAd()
+                        return
+                    }
+                    override fun onAdClicked() {
+                        // Called when a click is recorded for an ad.
+                    }
+                    override fun onAdImpression(impressionData: ImpressionData?) {
+                        // Called when an impression is recorded for an ad.
+                    }
+                })
+                show(this@MainActivity)
+            }
+        } else {
+            return
+        }
+    }
     override fun showVideoPlayerFragment(channel: Channel, selectedEpgDb: Epg?) {
+        showInterAd()
         launchFragment(ExoPlayerFragment.newInstance(channel, selectedEpgDb))
     }
 
@@ -39,6 +96,10 @@ class MainActivity : AppCompatActivity(), Navigator {
         onBackPressed()
     }
 
+    override fun onResume() {
+        super.onResume()
+        loadInterAd()
+    }
     @SuppressLint("CommitTransaction")
     private fun launchFragment(fragment: Fragment) {
         supportFragmentManager
